6d91d87152c95ce120c1f49a63788820f4930247
Mohab999 <mohab@gmail.com>
2018-11-27 15:52:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,13 +52,4 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 }
  
 });
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Marley | &new`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
 client.login(process.env.BOT_TOKEN);
